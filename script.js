8118ac40545cb3495a9f995ef4002c490a4f4332
Grasiela Texeira <grasy.mst@gmail.com>
2023-06-22 14:24:53 -0300

    Finalização do Projeto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.busca').addEventListener('submit', (event) => {
+document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
@@ -6,8 +6,55 @@ document.querySelector('.busca').addEventListener('submit', (event) => {
     if(input !== '') {
         showWarning('Carregando...')
 
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=70429059bc51c55375a45e1446e2d2c9&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+       
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            });
+
+            
+        }else {
+            clearInfo();
+            showWarning('Não encontramos essa localização');
+
+        }
+    } else {
+        clearInfo();
     }
 });
+
+function showInfo(json) {
+    showWarning('');
+
+   
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
 
 function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg;
